955ea0cb8b4686b4c60fbc42f54fc1c2e085f384
Beets <ddd>
2024-02-23 22:44:22 +1100

    2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 export const viewport = {
-	width: 'device-width',
-	initialScale: 1.0,
+  width: 'device-width',
+  initialScale: 1.0,
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }){
-	return (
-		<html lang="en">
-		<body>{children}</body>
-		</html>
-	);
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,33 @@
-import { getFrameMetadata} from 'coinbase/onchainkit';
+import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
 const frameMetadata = getFrameMetadata({
-	buttons: [
-		{
-			label: "Begin"
-		}
-	]
-	image: '$process.evn.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmXmHQhnYoWwZAqV1778UyBQN7gdNcCuze3Fm3bL1HzVcx/newsletter.mp4}',
-	post_url: '${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id1',
+  buttons: [
+    {
+      label: "Begin"
+    }
+  ],
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/0.png`,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
 export const metadata: Metadata = {
-	title: 'HERE & NOW NEWSLETTER',
-	description: 'A newsletter',
-	opengraph: {
-		title: 'HERE & NOW NEWSLETTER', 
-		images: ['${process.evn.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmZpvstBtTsxkpfEaBghLorVYcvLKVnx2tK5Y8BBZ5RPug/1.jpg}']
-	},
-	other: {
-		...frameMetadata
-	},
+  title: 'Cosmic Cowboys',
+  description: 'A frame telling the story of Cosmic Cowboys',
+  openGraph: {
+    title: 'Cosmic Cowboys',
+    description: 'A frame telling the story of Cosmic Cowboys',
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/0.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
 };
+
+export default function Page() {
+  return (
+    <>
+      <h1>Cosmic Cowboys</h1>
+    </>
+  );
+}
